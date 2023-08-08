7d8ef1350b4470e534854aee1397a9482f88de0f
himanshu7377 <him.singh7069@gmail.com>
2023-08-09 01:54:11 +0530

    home page

--- broki/src/App.js
+++ broki/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from './component/LoginPage';
+import HomePage from './component/HomePage'; 
 // Import other components and routes
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/" element={<LoginPage />} />
+        <Route path="/home" element={<HomePage />} />
         {/* Define other routes */}
       </Routes>
     </Router>
---
+++ broki/src/component/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/HomePage.css'; // Import the CSS file for styling
+
+const HomePage = () => {
+  return (
+    <div className="home-container">
+      <h2>Property Listings</h2>
+      {/* Your property listings content goes here */}
+    </div>
+  );
+};
+
+export default HomePage;
